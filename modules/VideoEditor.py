e3d07390ae9db2ee83c142d361bbd0eebd1e2491
arseniiarsenii <arseniivelichko2@gmail.com>
2021-06-11 13:41:37 +0300

    moved video operations into VideoEditor.py, refactored it

---
+++ modules/VideoEditor.py
@@ -0,0 +1,56 @@
+import logging
+import os
+import subprocess
+
+
+class VideoEditor:
+    """Handles video related operations, such as intro concatenation"""
+
+    @staticmethod
+    def concatenate(video_path: str, intro_path: str = "media/intro.mp4", delete_source: bool = False) -> str:
+        """
+        concatenating two videos (intro with the main video) if needed. Intro is to be placed in media folder.
+        """
+        logging.info(f"Concatenating video {video_path} with intro {intro_path}")
+
+        for path in [video_path, intro_path]:
+            if not os.path.exists(intro_path):
+                logging.error(f"File {path} not found. Cannot concatenate.")
+                return ""
+
+        concat_string = "file \'" + intro_path + "/media/intro.mp4\'\nfile \'" + video_path + "\'"
+        # it should look like:
+        #   file './media/intro.mp4'
+        #   file './media/vid.mp4'
+        with open("output/vidlist.txt", "w") as f:
+            f.write(concat_string)
+            f.close()
+
+        filename = "".join(video_path.split(".")[:-1])
+        extension = video_path.split(".")[-1]
+        concat_filename = f"{filename}_concatenated.{extension}"
+        concat_command = f"ffmpeg -f concat -safe 0 -i output/vidlist.txt -c copy {concat_filename}"
+        # line looks like: ffmpeg -f concat -safe 0 -i vidlist.txt -c copy output.mp4. More on ffmpeg.org
+
+        concat_process = subprocess.Popen(
+            "exec " + concat_command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+        )  # subprocess to execute ffmpeg utility command in shell and obtain all the flows
+
+        concat_process.stdout.readline()  # wait till the process finishes
+
+        # remove source files in necessary
+        if delete_source:
+            VideoEditor._remove_files(video_path)
+
+        return concat_filename  # return new filename
+
+    @staticmethod
+    def _remove_files(*files) -> None:
+
+        logging.info(f"Removing files: {files}")
+        for file in files:
+            os.remove(file)
--- modules/external_io_operations.py
+++ modules/external_io_operations.py
@@ -1,5 +1,4 @@
 import logging
-import os
 import subprocess
 import threading
 import typing as tp
@@ -8,6 +7,7 @@ from abc import ABC
 import ipfshttpclient
 from pinatapy import PinataPy
 
+from modules.VideoEditor import VideoEditor
 from modules.short_url_generator import update_short_url
 
 
@@ -17,63 +17,14 @@ class ExternalIoGateway:
         self.config = config
         self.ipfs_hash: tp.Optional[str] = None
 
-    @staticmethod
-    def concatenate(dirname: str, filename: str) -> str:
-        """
-        :param dirname: path to the project ending with .../cameras_robonomics
-        :type dirname: str
-        :param filename: full name of a recorded video
-        :type filename: str
-        :return: full name of a new video (concatenated with intro)
-        :rtype: str
-
-        concatenating two videos (intro with a main video) if needed. Intro is to be placed in media folder.
-        More in config file
-        """
-        logging.info("Concatenating videos")
-        if not os.path.exists(dirname + "/media/intro.mp4"):
-            raise Exception("Intro file doesn't exist!")
-        concat_string = "file \'" + dirname + "/media/intro.mp4\'\nfile \'" + filename + "\'"
-        # it should look like:
-        #   file './media/intro.mp4'
-        #   file './media/vid.mp4'
-        with open(dirname + "/output/vidlist.txt", "w") as text_file:
-            text_file.write(concat_string)
-            text_file.close()  # create txt file
-        concat_filename = filename[:-4] + "_intro" + filename[
-                                                     -4:]  # new file will have another name to detect concatenated
-        # videos
-        concat_command = (
-                "ffmpeg -f concat -safe 0 -i "
-                + dirname
-                + "/output/vidlist.txt -c copy "
-                + concat_filename
-        )  # line looks like: ffmpeg -f concat -safe 0 -i vidlist.txt -c copy output.mp4. More on ffmpeg.org
-        concat_process = subprocess.Popen(
-            "exec " + concat_command,
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            stdin=subprocess.PIPE,
-        )  # subprocess to execute ffmpeg utility command in shell and obtain all the flows
-        concat_process.stdout.readline()  # wait till the process finishes
-        return concat_filename  # return new filename
-
-    def send(self, filename: str, keyword: str = "", qrpic: str = "") -> str:
-        """
-        :param filename: full name of a recorded video
-        :type filename: str
-        :param keyword: shorturl keyword. More on yourls.org. E.g. url.today/6b. 6b is a keyword
-        :type keyword: str
-        :param qrpic: name of a qr-code file. Qr-code, which is redirecting to IPFS gateway
-        :type qrpic: str
-
-        concatenate if needed, publish files to external_io locally, send them to pinata, push hashes to robonomics
-        """
+    def send(self, filename: str, keyword: str = "") -> str:
+        """Handle external IO operations, such as IPFS and Robonomics interactions"""
         if self.config["intro"]["enable"]:
             try:
-                non_concatenated_filename = filename  # save old filename to delete if later
-                filename = filename  # get concatenated video filename
+                filename = VideoEditor.concatenate(
+                    filename,
+                    delete_source=self.config["general"]["delete_after_record"]
+                )  # get concatenated video filename
             except Exception as e:
                 logging.error("Failed to concatenate. Error: ", e)
 
@@ -91,12 +42,6 @@ class ExternalIoGateway:
                     "Error while publishing to IPFS or pinning to pinata. Error: ", e
                 )
 
-        if self.config["general"]["delete_after_record"] and qrpic:
-            try:
-                self.clean_after_record(filename, qrpic, non_concatenated_filename)
-            except Exception as e:
-                logging.error("Error while deleting file, error: ", e)
-
         if self.config["datalog"]["enable"] and self.config["external_io"]["enable"]:
             try:
                 worker = RobonomicsWorker(self, self.config)
@@ -105,13 +50,6 @@ class ExternalIoGateway:
                 logging.error("Error while sending IPFS hash to chain, error: ", e)
 
             return self.ipfs_hash
-
-    def clean_after_record(self, filename: str, qrpic: str, non_concatenated_filename: str):
-        logging.info("Removing files")
-        os.remove(filename)
-        os.remove(qrpic)
-        if self.config["intro"]["enable"]:
-            os.remove(non_concatenated_filename)  # liberate free space. delete both concatenated and initial files
 
 
 class BaseIoWorker(ABC):
