340f3dfe582946458f049e51e8318f1a75a57b55
Aboba <timofeevnik41@gmail.com>
2021-06-17 02:45:19 +0300

    :gem: unit now getting its own internal_id

--- modules/Unit.py
+++ modules/Unit.py
@@ -1,6 +1,8 @@
+import csv
 import logging
 import os
 import typing as tp
+from collections import defaultdict
 from datetime import datetime as dt
 from uuid import uuid4
 
@@ -17,8 +19,8 @@ class Unit:
 
     def __init__(self, config: Config, uuid: str = "") -> None:
         self.uuid: str = uuid or self._generate_uuid()
-        self.internal_id: str = self._get_internal_id
-        self.employee: tp.Union[Employee, None] = None
+        self.internal_id: int = self._get_internal_id()
+        self.employee: tp.Optional[Employee] = None
         self.product_data: tp.Optional[ProductData] = self._get_product_data()
         self.passport = Passport(self)
         self._keyword = ""
@@ -33,11 +35,18 @@ class Unit:
     def _generate_uuid() -> str:
         return uuid4().hex
 
-    # todo
-    def _get_internal_id(self) -> str:
+    def _get_internal_id(self) -> int:
         """get own internal id using own uuid"""
+        ids_dict = self._load_internal_ids()
 
-        pass
+        if not len(ids_dict):
+            self._save_internal_id(self.uuid, 1)
+            return 1
+
+        internal_id = list(ids_dict.values())[-1] + 1
+        self._save_internal_id(self.uuid, internal_id)
+
+        return internal_id
 
     def _get_product_data(self) -> ProductData:
         filename = f"unit-passports/unit-passport-{self.uuid}.yaml"
@@ -52,6 +61,25 @@ class Unit:
             product_data = {}
 
         return product_data
+
+    def _load_internal_ids(self, path: str = "config/internal_ids") -> tp.Dict[str, int]:
+        """Loads internal ids matching table, returns dict in format {uuid: internal_id}"""
+        internal_ids = {}
+
+        with open(path, "r", newline="") as f:
+            data = csv.reader(f, delimiter=";")
+            for uuid, id in data:
+                internal_ids[uuid] = id
+
+        return internal_ids
+
+    def _save_internal_id(self, uuid: str, internal_id: int, path: str = "config/internal_ids"):
+        """Saves internal id matching table, returns dict in format {uuid: internal_id}"""
+        with open(path, "w", newline="") as f:
+            writer = csv.writer(f, delimiter=";")
+            writer.writerow([uuid, internal_id])
+
+        logging.debug(f"Saved {uuid[:6]}:{internal_id} to matching table")
 
     def end_session(self) -> None:
         """wrap up the session when video recording stops an save video data as well as session end timestamp"""
