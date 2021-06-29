91f8d1740bb5eb84eed5323fad46773af66f30aa
Aboba <timofeevnik41@gmail.com>
2021-06-29 15:36:18 +0300

    :gem: removed tp.Any from cfg type

--- feecc_hub/_Types.py
+++ feecc_hub/_Types.py
@@ -1,3 +1,6 @@
 import typing as tp
 
-Config = tp.Dict[str, tp.Dict[str, tp.Any]]
+Config = tp.Dict[
+    tp.Union[int, str], tp.Dict[tp.Union[int, str], tp.Optional[tp.Union[
+        str, float, bytes, tp.Dict[
+            tp.Union[int, str], tp.Optional[tp.Dict[tp.Union[int, str], tp.Optional[tp.Union[str, float, bytes]]]]]]]]]
