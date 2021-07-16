999339f2b860ff7392113bccab1170e3059a722a
Aboba <timofeevnik41@gmail.com>
2021-07-17 00:22:49 +0300

    :hammer: Tests now working w/o app

--- test_api.py
+++ test_api.py
@@ -1,18 +1,22 @@
-import time
+from fastapi.testclient import TestClient
+
+from app import api
 
 import requests
+
+client = TestClient(api)
 
 TEST_SERVER = "http://127.0.0.1:5000"
 
 
 def get_unit(server_address: str) -> requests.Response:
-    resp = requests.post(server_address + "/api/unit/new", json={"unit_type": "cryptoanalyzer"})
+    resp = client.post(server_address + "/api/unit/new", json={"unit_type": "cryptoanalyzer"})
     return resp
 
 
 def test_employee_login() -> None:
     """Test to check if employee could be logged in system"""
-    resp = requests.post(
+    resp = client.post(
         TEST_SERVER + "/api/employee/log-in",
         json={"workbench_no": 1, "employee_rfid_card_no": "0008368511"},
     )
@@ -36,7 +40,7 @@ def test_unit_creation() -> None:
 def test_unit_record_logged_in_employee() -> None:
     """Test to check if recording could be started when employee is logged in"""
     unit_id = unit.json()["unit_internal_id"]
-    resp = requests.post(
+    resp = client.post(
         TEST_SERVER + f"/api/unit/{unit_id}/start",
         json={
             "workbench_no": 1,
@@ -52,7 +56,7 @@ def test_unit_record_logged_in_employee() -> None:
 def test_unit_stop_record_logged_employee() -> None:
     """Test to check if recording could be stopped when employee is logged in"""
     unit_id = unit.json()["unit_internal_id"]
-    resp = requests.post(
+    resp = client.post(
         TEST_SERVER + f"/api/unit/{unit_id}/end",
         json={"workbench_no": 1, "additional_info": {"test": "test"}},
     )
@@ -64,7 +68,7 @@ def test_unit_stop_record_logged_employee() -> None:
 def test_unit_record_second_stage() -> None:
     """Test to check if recording could be started when employee is logged in"""
     unit_id = unit.json()["unit_internal_id"]
-    resp = requests.post(
+    resp = client.post(
         TEST_SERVER + f"/api/unit/{unit_id}/start",
         json={
             "workbench_no": 1,
@@ -80,7 +84,7 @@ def test_unit_record_second_stage() -> None:
 def test_unit_stop_record_second_stage() -> None:
     """Test to check if recording could be stopped when employee is logged in"""
     unit_id = unit.json()["unit_internal_id"]
-    resp = requests.post(
+    resp = client.post(
         TEST_SERVER + f"/api/unit/{unit_id}/end",
         json={"workbench_no": 1, "additional_info": {"test": "test"}},
     )
@@ -92,7 +96,7 @@ def test_unit_stop_record_second_stage() -> None:
 def test_unit_upload_logged_employee() -> None:
     """Test to check if recording could be uploaded when employee is logged in"""
     unit_id = unit.json()["unit_internal_id"]
-    resp = requests.post(
+    resp = client.post(
         TEST_SERVER + f"/api/unit/{unit_id}/upload",
         json={"workbench_no": 1},
     )
@@ -103,7 +107,7 @@ def test_unit_upload_logged_employee() -> None:
 
 def test_employee_logout() -> None:
     """Test to check if employee could be logged out"""
-    logout_resp = requests.post(TEST_SERVER + "/api/employee/log-out", json={"workbench_no": 1})
+    logout_resp = client.post(TEST_SERVER + "/api/employee/log-out", json={"workbench_no": 1})
 
     assert logout_resp.ok, f"{logout_resp.json()}"
     assert logout_resp.json()["status"] is True, f"{logout_resp.json()}"
@@ -112,7 +116,7 @@ def test_employee_logout() -> None:
 def test_unit_record_not_logged_in_employee() -> None:
     """Test to check if recording couldn't be started when employee not logged in"""
     unit_id = unit.json()["unit_internal_id"]
-    resp = requests.post(
+    resp = client.post(
         TEST_SERVER + f"/api/unit/{unit_id}/start",
         json={
             "workbench_no": 1,
@@ -126,7 +130,7 @@ def test_unit_record_not_logged_in_employee() -> None:
 
 def test_fake_employee_login() -> None:
     """Test to check if fake employee couldn't be logged in system"""
-    resp = requests.post(
+    resp = client.post(
         TEST_SERVER + "/api/employee/log-in",
         json={"workbench_no": 2, "employee_rfid_card_no": "0000000000"},
     )
@@ -138,14 +142,14 @@ def test_fake_employee_login() -> None:
 
 def test_employee_fake_logout() -> None:
     """Test to check if unauthorized employee couldn't be logged out"""
-    logout_resp = requests.post(TEST_SERVER + "/api/employee/log-out", json={"workbench_no": 1})
+    logout_resp = client.post(TEST_SERVER + "/api/employee/log-out", json={"workbench_no": 1})
 
     assert logout_resp.json()["status"] is False, f"{logout_resp.json()}"
 
 
 def test_workbench_status_handler() -> None:
     """Test to check if unauthorized employee couldn't be logged out"""
-    status_resp = requests.get(TEST_SERVER + "/api/workbench/1/status")
+    status_resp = client.get(TEST_SERVER + "/api/workbench/1/status")
 
     assert status_resp.ok, f"{status_resp.json()}"
     assert status_resp.json() is not None, f"{status_resp.json()}"
@@ -153,14 +157,14 @@ def test_workbench_status_handler() -> None:
 
 def test_api_integrate() -> None:
     def check_state(expected: str) -> None:
-        current_state = requests.get(TEST_SERVER + "/api/workbench/2/status").json()["state"]
+        current_state = client.get(TEST_SERVER + "/api/workbench/2/status").json()["state"]
         assert (
             current_state == expected
         ), f"Failed to assert state. expected {expected}, got {current_state}"
 
     check_state("AwaitLogin")
 
-    login_resp = requests.post(
+    login_resp = client.post(
         TEST_SERVER + "/api/employee/log-in",
         json={"workbench_no": 2, "employee_rfid_card_no": "0008368511"},
     )
@@ -177,7 +181,7 @@ def test_api_integrate() -> None:
 
     test_unit_id = test_unit.json()["unit_internal_id"]
 
-    unit_start_resp = requests.post(
+    unit_start_resp = client.post(
         TEST_SERVER + f"/api/unit/{test_unit_id}/start",
         json={
             "workbench_no": 2,
@@ -192,7 +196,7 @@ def test_api_integrate() -> None:
         "status"
     ], f"Got error while starting operation: {unit_start_resp.json()}"
 
-    unit_stop_resp = requests.post(
+    unit_stop_resp = client.post(
         TEST_SERVER + f"/api/unit/{test_unit_id}/end",
         json={"workbench_no": 2, "additional_info": {"test": "test"}},
     )
@@ -203,7 +207,7 @@ def test_api_integrate() -> None:
         "status"
     ], f"Got error while stopping operation: {unit_stop_resp.json()}"
 
-    unit_upload_resp = requests.post(
+    unit_upload_resp = client.post(
         TEST_SERVER + f"/api/unit/{test_unit_id}/upload",
         json={"workbench_no": 2},
     )
@@ -212,7 +216,7 @@ def test_api_integrate() -> None:
         "status"
     ], f"Got error while wrapping up session: {unit_upload_resp.json()}"
 
-    logout_resp = requests.post(TEST_SERVER + "/api/employee/log-out", json={"workbench_no": 2})
+    logout_resp = client.post(TEST_SERVER + "/api/employee/log-out", json={"workbench_no": 2})
 
     check_state("AwaitLogin")
 
