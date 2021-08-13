00fb9072953f683170f48470bea015182c87ab30
arseniiarsenii <arseniivelichko2@gmail.com>
2021-08-13 18:41:48 +0300

    bugfix

--- feecc_hub_src/_logging.py
+++ feecc_hub_src/_logging.py
@@ -2,21 +2,23 @@ import sys
 
 # set up logging configurations
 BASE_LOGGING_CONFIG = {
-    "level": "DEBUG",
     "colorize": True,
     "backtrace": True,
     "diagnose": True,
     "catch": True,
-    "rotation": "10 MB",
-    "compression": "zip",
 }
 
 # logging settings for the console logs
-CONSOLE_LOGGING_CONFIG = {**BASE_LOGGING_CONFIG, "sink": sys.stdout, "level": "INFO"}
+CONSOLE_LOGGING_CONFIG = {
+    **BASE_LOGGING_CONFIG,  # type: ignore
+    "level": "INFO",
+    "sink": sys.stdout,
+}
 
 # logging settings for the log file
 FILE_LOGGING_CONFIG = {
-    **BASE_LOGGING_CONFIG,
+    **BASE_LOGGING_CONFIG,  # type: ignore
+    "level": "DEBUG",
     "sink": "hub.log",
     "rotation": "10 MB",
     "compression": "zip",
