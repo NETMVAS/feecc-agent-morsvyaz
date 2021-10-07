5a5804d78910dab9378f94ebd31a4d8e9d4e3d3e
arseniiarsenii <arseniivelichko2@gmail.com>
2021-10-07 19:53:04 +0300

    removed unused code

--- feecc_hub_src/feecc_hub/exceptions.py
+++ feecc_hub_src/feecc_hub/exceptions.py
@@ -1,24 +1,8 @@
-class EmployeeUnauthorizedError(Exception):
-    pass
-
-
 class UnitNotFoundError(Exception):
     pass
 
 
-class OperationNotFoundError(Exception):
-    pass
-
-
-class WorkbenchNotFoundError(Exception):
-    pass
-
-
 class EmployeeNotFoundError(Exception):
-    pass
-
-
-class CameraNotFoundError(Exception):
     pass
 
 
