84d5054a7bfb3d943d7dc670c9a3114c326e88c5
arseniiarsenii <arseniivelichko2@gmail.com>
2021-10-26 14:16:26 +0300

    reworked shorturl generation

--- src/feecc_workbench/_short_url_generator.py
+++ src/feecc_workbench/_short_url_generator.py
@@ -1,10 +1,14 @@
+import typing as tp
+
 import httpx
 from loguru import logger
 
 from .config import config
 
+YOURLS_CONFIG = config.yourls
 
-def generate_short_url() -> str:
+
+def generate_short_url(underlying_url: tp.Optional[str] = None) -> str:
     """
     :return keyword: shorturl keyword. More on yourls.org. E.g. url.today/6b. 6b is a keyword
     :return link: full yourls url. E.g. url.today/6b
@@ -13,54 +17,36 @@ def generate_short_url() -> str:
     just to print the qr, later the redirect link is updated with a gateway link to the video
     """
     logger.debug("Generating dummy short url to replace with actual link later")
-
-    config_ = config.yourls
-    url = f"https://{config_.server}/yourls-api.php"
+    url = f"https://{YOURLS_CONFIG.server}/yourls-api.php"
     querystring = {
-        "username": config_.username,
-        "password": config_.password,
+        "username": YOURLS_CONFIG.username,
+        "password": YOURLS_CONFIG.password,
         "action": "shorturl",
         "format": "json",
-        "url": "example.com",
+        "url": underlying_url or "example.com",
     }  # api call to the yourls server. More on yourls.org
-
-    try:
-        response = httpx.get(url, params=querystring)
-        logger.debug(f"{config_.server} returned: {response.text}")
-        keyword: str = response.json()["url"]["keyword"]
-        link = str(config_.server) + "/" + keyword  # link of form url.today/6b
-        logger.info(f"Assigned yourls link: {link}")
-        return link
-    except Exception as E:
-        logger.error(f"Failed to create URL, replaced by fake link (url.today/55). Error: {E}")
-        return "url.today/55"
+    response = httpx.get(url, params=querystring)
+    logger.debug(f"{YOURLS_CONFIG.server} returned: {response.text}")
+    keyword: str = response.json()["url"]["keyword"]
+    link = "https://" + str(YOURLS_CONFIG.server) + "/" + keyword  # link of form url.today/6b
+    logger.info(f"Assigned yourls link: {link}")
+    return link
 
 
-def update_short_url(keyword: str, ipfs_hash: str) -> None:
-    """
-    :param keyword: short url keyword. More on yourls.org. E.g. url.today/6b. 6b is a keyword
-    :type keyword: str
-    :param ipfs_hash: IPFS hash of a recorded video
-    :type ipfs_hash: str
-
-    Update redirecting service so that now the short url points to the  gateway to a video in external_io
-    """
-    url = f"https://{config.ipfs}/yourls-api.php"
-    new_file_url: str = f"{config.ipfs}{ipfs_hash}"
+def update_short_url(short_url: str, new_url: str) -> None:
+    """Update redirecting service so that now the short url points to the  gateway to a video in external_io"""
+    keyword = short_url.split("/")[-1]
+    url = f"https://{YOURLS_CONFIG.server}/yourls-api.php"
     params = {
-        "username": config.yourls.username,
-        "password": config.yourls.password,
+        "username": YOURLS_CONFIG.username,
+        "password": YOURLS_CONFIG.password,
         "action": "update",
         "format": "json",
-        "url": new_file_url,
+        "url": new_url,
         "shorturl": keyword,
     }
+    response = httpx.get(url, params=params)
+    logger.debug(f"Trying to update short url link. Keyword: {keyword}")
 
-    try:
-        response = httpx.get(url, params=params)
-        logger.debug(f"Trying to update short url link. Keyword: {keyword}")
-
-        if response.status_code != 200:
-            logger.warning("Failed to update short url link")
-    except Exception as E:
-        logger.error(f"Failed to update URL: {E}")
+    if response.status_code != 200:
+        logger.warning("Failed to update short url link")
