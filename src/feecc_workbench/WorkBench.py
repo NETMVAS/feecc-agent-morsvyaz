f47884c4fea7a1ea3cdbd9b790b67f376a883a24
arseniiarsenii <arseniivelichko2@gmail.com>
2022-05-08 21:31:53 +0300

    improved logging

--- src/feecc_workbench/WorkBench.py
+++ src/feecc_workbench/WorkBench.py
@@ -44,7 +44,7 @@ class WorkBench(metaclass=SingletonMeta):
 
         logger.info(f"Workbench {self.number} was initialized")
 
-    @logger.catch(reraise=True)
+    @logger.catch(reraise=True, exclude=StateForbiddenError)
     async def create_new_unit(self, schema: ProductionSchema) -> Unit:
         """initialize a new instance of the Unit class"""
         if self.state != State.AUTHORIZED_IDLING_STATE:
@@ -76,7 +76,7 @@ class WorkBench(metaclass=SingletonMeta):
         logger.info(f"Workbench no.{self.number} state changed: {self.state.value} -> {new_state.value}")
         self.state = new_state
 
-    @logger.catch(reraise=True)
+    @logger.catch(reraise=True, exclude=StateForbiddenError)
     def log_in(self, employee: Employee) -> None:
         """authorize employee"""
         self._validate_state_transition(State.AUTHORIZED_IDLING_STATE)
@@ -86,7 +86,7 @@ class WorkBench(metaclass=SingletonMeta):
 
         self.switch_state(State.AUTHORIZED_IDLING_STATE)
 
-    @logger.catch(reraise=True)
+    @logger.catch(reraise=True, exclude=StateForbiddenError)
     def log_out(self) -> None:
         """log out the employee"""
         self._validate_state_transition(State.AWAIT_LOGIN_STATE)
@@ -99,7 +99,7 @@ class WorkBench(metaclass=SingletonMeta):
 
         self.switch_state(State.AWAIT_LOGIN_STATE)
 
-    @logger.catch(reraise=True)
+    @logger.catch(reraise=True, exclude=StateForbiddenError)
     def assign_unit(self, unit: Unit) -> None:
         """assign a unit to the workbench"""
         self._validate_state_transition(State.UNIT_ASSIGNED_IDLING_STATE)
@@ -118,7 +118,7 @@ class WorkBench(metaclass=SingletonMeta):
         else:
             self.switch_state(State.UNIT_ASSIGNED_IDLING_STATE)
 
-    @logger.catch(reraise=True)
+    @logger.catch(reraise=True, exclude=StateForbiddenError)
     def remove_unit(self) -> None:
         """remove a unit from the workbench"""
         self._validate_state_transition(State.AUTHORIZED_IDLING_STATE)
@@ -129,7 +129,7 @@ class WorkBench(metaclass=SingletonMeta):
 
         self.switch_state(State.AUTHORIZED_IDLING_STATE)
 
-    @logger.catch(reraise=True)
+    @logger.catch(reraise=True, exclude=StateForbiddenError)
     async def start_operation(self, additional_info: AdditionalInfo) -> None:
         """begin work on the provided unit"""
         self._validate_state_transition(State.PRODUCTION_STAGE_ONGOING_STATE)
@@ -143,7 +143,7 @@ class WorkBench(metaclass=SingletonMeta):
 
         self.switch_state(State.PRODUCTION_STAGE_ONGOING_STATE)
 
-    @logger.catch(reraise=True)
+    @logger.catch(reraise=True, exclude=StateForbiddenError)
     async def assign_component_to_unit(self, component: Unit) -> None:
         """assign provided component to a composite unit"""
         assert (
@@ -158,7 +158,7 @@ class WorkBench(metaclass=SingletonMeta):
 
             self.switch_state(State.UNIT_ASSIGNED_IDLING_STATE)
 
-    @logger.catch(reraise=True)
+    @logger.catch(reraise=True, exclude=StateForbiddenError)
     async def end_operation(self, additional_info: tp.Optional[AdditionalInfo] = None, premature: bool = False) -> None:
         """end work on the provided unit"""
         self._validate_state_transition(State.UNIT_ASSIGNED_IDLING_STATE)
@@ -191,7 +191,7 @@ class WorkBench(metaclass=SingletonMeta):
 
         self.switch_state(State.UNIT_ASSIGNED_IDLING_STATE)
 
-    @logger.catch(reraise=True)
+    @logger.catch(reraise=True, exclude=StateForbiddenError)
     async def upload_unit_passport(self) -> None:
         """upload passport file into IPFS and pin it to Pinata, publish hash to Robonomics"""
         assert self.unit is not None, "No unit is assigned to the workbench"
