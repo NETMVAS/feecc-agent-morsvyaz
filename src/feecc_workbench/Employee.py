d3c6230783138b7b096339b9b633a2e535eb3c0b
arseniiarsenii <arseniivelichko2@gmail.com>
2022-06-01 13:27:24 +0300

    minor update to passport code logic

--- src/feecc_workbench/Employee.py
+++ src/feecc_workbench/Employee.py
@@ -10,7 +10,8 @@ class Employee:
     passport_code: str = ""
 
     def __post_init__(self) -> None:
-        self.passport_code = self.get_passport_code()
+        if not self.passport_code:
+            self.passport_code = self.get_passport_code()
 
     @property
     def data(self) -> dict[str, str]:
@@ -28,5 +29,4 @@ class Employee:
 
         employee_passport_string: str = " ".join([self.rfid_card_id, self.name, self.position])
         employee_passport_string_encoded: bytes = employee_passport_string.encode()
-        employee_passport_code: str = hashlib.sha256(employee_passport_string_encoded).hexdigest()
-        return employee_passport_code
+        return hashlib.sha256(employee_passport_string_encoded).hexdigest()
