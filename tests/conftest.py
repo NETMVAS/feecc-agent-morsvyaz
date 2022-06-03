d84969b17a6d6e80450a458b1f657b047d51ccef
arseniiarsenii <arseniivelichko2@gmail.com>
2022-06-04 02:56:24 +0300

    improved type annotations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,15 @@
+from typing import no_type_check
+
 import pytest
 
 
-def pytest_addoption(parser):
+@no_type_check
+def pytest_addoption(parser) -> None:
     parser.addoption("--short-url", action="store_true", default=False, help="run short url generator tests")
 
 
-def pytest_collection_modifyitems(config, items):
+@no_type_check
+def pytest_collection_modifyitems(config, items) -> None:
     if config.getoption("--short-url"):
         return
 
